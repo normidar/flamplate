c2d4a6b242152a1205eb75c50b04bdeb09605b2c
劉達 <>
2022-08-10 23:14:20 +0900

    u

--- lib/games/biginer/base_text/draw_text.dart
+++ lib/games/biginer/base_text/draw_text.dart
@@ -4,14 +4,17 @@ import 'package:flutter/material.dart';
 
 /// This class encapulates the whole game.
 class DrawText extends FlameGame {
+  TextComponent? text;
   @override
   Future<void>? onLoad() async {
     await super.onLoad();
     TextStyle defaultTextStyle =
         const TextStyle(fontSize: 10, color: Colors.white);
-    var text = TextComponent(textRenderer: TextPaint(style: defaultTextStyle))
+    text = TextComponent(textRenderer: TextPaint(style: defaultTextStyle))
       ..position = Vector2(50, 50)
       ..text = 'TEXT';
-    add(text);
+    add(text!);
+    text?.text = 'TEXT2';
   }
+
 }
